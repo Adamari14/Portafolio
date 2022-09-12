551d58c83a9b3d8347b56e1e93dffa146d42b773
adamari14 <adamarim_15@hotmail.com>
2022-09-12 09:52:42 -0500

    Editing the section "Who am I?"

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -9,22 +9,21 @@ const About = () => {
           <div className="md:mx-4">
             <h3 className="text-2xl text-gray-800 font-bold mb-4">Who am I?</h3>
             <p className="text-sm text-gray-400 leading-6 mb-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima
-              amet accusantium provident, dignissimos corporis reprehenderit
-              numquam nobis ex dolore? Maxime veritatis aliquam amet inventore
-              recusandae nulla incidunt tenetur repellat voluptatibus molestias
-              quas deleniti quia vel officia veniam expedita exercitationem
-              modi, maiores saepe ut dolorum. Voluptas optio laudantium pariatur
-              aperiam!
+              My name is Adamari Lopez. I am Mechatronics engineering student, professional in training, with knowledge in
+              areas of mechanical engineering, electronics and computing, ability to apply tools
+              specialized computational skills and quick learning of any tool required for the job,
+              team management and work disposition.
             </p>
             <p className="text-sm text-gray-400 leading-6 mb-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima
-              amet accusantium provident, dignissimos corporis reprehenderit
-              numquam nobis ex dolore? Maxime veritatis aliquam amet inventore
-              recusandae nulla incidunt tenetur repellat voluptatibus molestias
-              quas deleniti quia vel officia veniam expedita exercitationem
-              modi, maiores saepe ut dolorum. Voluptas optio laudantium pariatur
-              aperiam!
+              Willing to work in activities that implement manufacturing processes (machinery management and
+              industrial equipment, processing operations, conventional and advanced manufacturing processes),
+              communication (reading comprehension, oral and written expression, critical reading, writing texts), drawing
+              computer aided, probability and statistics (sampling, distributions), programming
+              (structured, control structures, visual programming, hardware/software applications), circuits
+              electrical, analog electronics (timer circuits, printed circuit design and construction),
+              digital electronics (registers and counters, analysis of logic circuits, digital systems), computer electronics
+              power, metrology (measurement, standards and normalization, sensors and transducers).
+              Social and labor responsibility, as well as discipline, ethics, honesty and loyalty.
             </p>
           </div>
           <Skills />
